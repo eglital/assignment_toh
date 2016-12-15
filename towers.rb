2dfc4ab49a718e42b231b7768d4088b31224551c
Egle Libby <egle.ciparyte@gmail.com>
2016-12-15 17:18:42 +0200

    update render method for better visualization and fix minor issues

--- towers.rb
+++ towers.rb
@@ -14,20 +14,30 @@ class TowerofHanoi
     @towers = height
     @board = [(1..@towers).to_a, [], []]
     @original = @board[0].dup
-    @player_input = ""
-    @player_move = []
+    @player_move = ""
   end
 
   def render
     puts "Current board: "
-    puts @board.inspect
+    @towers.times do |i|
+      level_string = ""
+      @board.each do |row|
+        row_dup = row.dup
+        while row_dup.length < @towers
+          row_dup.unshift(0)
+        end
+        level_string += ("o" * row_dup[i] + " " * (@towers + 2 - row_dup[i]))
+      end
+      puts level_string
+    end
+    puts "1" + " " * (@towers + 1) + "2" + " " * (@towers + 1) + "3"
   end
 
   def is_valid?
-    if @player_input == "q"
+    if @player_move == "q"
       exit
     end
-    @player_move = @player_input.split(',')
+    @player_move = @player_move.split(',')
     if @player_move.length == 2
       @player_move = @player_move.map { |e|  e.to_i }
     else
@@ -38,7 +48,10 @@ class TowerofHanoi
       if @board[@player_move[1] - 1].empty?
         return true
       elsif @board[@player_move[0]-1].empty?
-        puts "You can not move a disk from an empty row."
+        puts "You can not move a disk from an empty column."
+        return false
+      elsif @board[@player_move[1]-1][0] == @board[@player_move[0]-1][0]
+        puts "Moving from the same column to the same one doesn't change anything"
         return false
       elsif @board[@player_move[1]-1][0] > @board[@player_move[0]-1][0]
         return true
@@ -53,17 +66,16 @@ class TowerofHanoi
   end
 
   def get_user_input
-    until @player_input.length > 0 && is_valid?
+    until @player_move.length > 0 && is_valid?
       puts "Enter move: "
       print "> "
-      @player_input = gets.chomp
+      @player_move = gets.chomp
     end
   end
 
   def move
     @board[@player_move[1]-1].unshift(@board[@player_move[0]-1].shift)
-    @player_input = ""
-    @player_move = []
+    @player_move = ""
   end
 
   def check_win?
@@ -74,22 +86,23 @@ class TowerofHanoi
 
     puts "Welcome to Tower of Hanoi!"
     puts "Instructions:"
-    puts "Enter where you'd like to move from and toin the format '1,3'. Enter 'q' to quit."
+    puts "Enter where you'd like to move from and to in the format '1,3'. Enter 'q' to quit."
     render
 
     until check_win? 
+
       get_user_input
 
       move
+
       render
 
-      puts "Congratulations! You won!" if check_win?
-
     end
+    puts "Congratulations! You won!"
     exit
   end
 
 end
 
-t = TowerofHanoi.new(3)
+t = TowerofHanoi.new(4)
 t.play
